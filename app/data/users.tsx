b14fdb05c424f8b13a16add844ea86787be76b17
pmwatt <prachnachai.mek@student.mahidol.edu>
2024-01-04 20:24:36 +0700

    split data into users and exams files

---
+++ app/data/users.tsx
@@ -0,0 +1,52 @@
+const users = [
+    {
+        id: 6688999,
+        password: 'Peter$12345678',
+        fname: 'Peter',
+        sname: 'Aekmepel',
+        imgsrc: 'https://firebasestorage.googleapis.com/v0/b/storage1-15612.appspot.com/o/6F00A038-886C-478C-925E-B38A1CE157E5_1_105_c.jpeg?alt=media&token=ca1db308-8c27-4dc5-ad74-62debfa04eda',
+        tel: '0834205948',
+        year: '1',
+        sec: '3'
+    },
+    {
+        id: 6588998,
+        password: '',
+        fname: 'Steve',
+        sname: 'Jobs',
+        imgsrc: 'https://firebasestorage.googleapis.com/v0/b/storage1-15612.appspot.com/o/IMG_2906.jpeg?alt=media&token=46d7132e-13d9-4a3a-9d16-67ff46ec5437',
+        tel: '09746219874',
+        year: '2',
+        sec: '2'
+    },
+    {
+        id: 6488997,
+        password: 'Peter$12345678',
+        fname: 'Ananta',
+        sname: 'Topanre',
+        imgsrc: 'https://firebasestorage.googleapis.com/v0/b/storage1-15612.appspot.com/o/image%2021.png?alt=media&token=19dcbaec-90cc-49d8-8ee8-76dcb1ecb1a3',
+        tel: '0682739897',
+        year: '3',
+        sec: '1'
+    },
+    {
+        id: 6388996,
+        password: 'Peter$12345678',
+        fname: 'Krit',
+        sname: 'Eakamai',
+        imgsrc: 'https://firebasestorage.googleapis.com/v0/b/storage1-15612.appspot.com/o/Notice.png?alt=media&token=0ff2b0f1-5f2f-496d-bdf3-02cbbb213758',
+        tel: '0881079403',
+        year: '4',
+        sec: '2'
+    },
+    {
+        id: 6688995,
+        password: 'Peter$12345678',
+        fname: 'Mai',
+        sname: 'Touristim',
+        imgsrc: 'https://firebasestorage.googleapis.com/v0/b/storage1-15612.appspot.com/o/phrae.png?alt=media&token=dbf22270-c60a-41c6-8b97-11751dcb02b0',
+        tel: '0911287584',
+        year: '1',
+        sec: '1'
+    }
+]
